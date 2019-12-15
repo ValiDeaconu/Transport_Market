635e6038b8b26ccc059b66f5bf9d22518823a587
Vali <valideaconu98@gmail.com>
2019-12-15 21:42:22 +0200

    Deleted HTMLReader

--- src/main/java/org/transexpress/snap/api/HomeController.java
+++ src/main/java/org/transexpress/snap/api/HomeController.java
@@ -1,11 +1,8 @@
 package org.transexpress.snap.api;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.transexpress.snap.misc.HTMLReader;
 
 @RequestMapping("")
 @RestController
